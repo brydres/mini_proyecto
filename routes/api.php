a88ee70face3e00443ce069bea509a2875c8741e
brydres <bryanb1489@gmail.com>
2023-09-25 12:26:46 -0500

    done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AsistenciaController;
+use App\Http\Controllers\ClaseController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\MatriculaController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("/usuarios", [UsuarioController::class, "index"]);
+Route::get("/usuarios/buscar/{id}", [UsuarioController::class, "getByID"]);
+Route::get("/usuarios/maestros", [UsuarioController::class, "showMaestros"]);
+Route::get("/usuarios/alumnos", [UsuarioController::class, "showAlumnos"]);
+Route::post("/usuarios/create", [UsuarioController::class, "create"]);
+Route::put("/usuarios/update/{id}", [UsuarioController::class, "update"]);
+Route::put("/usuarios/delete/{id}", [UsuarioController::class, "destroy"]);
+//
+Route::get("/cursos", [CursoController::class, "index"]);
+Route::post("/cursos/create", [CursoController::class, "create"]);
+Route::put("/cursos/update/{id}", [CursoController::class, "update"]);
+Route::put("/cursos/delete/{id}", [CursoController::class, "destroy"]);
+//
+Route::get("/matriculas", [MatriculaController::class, "index"]);
+Route::get("/matriculas/{id}", [MatriculaController::class, "showMatricula"]);
+Route::get("/clases", [ClaseController::class, "index"]);
+//
+Route::post("/inscribir/alumno", [MatriculaController::class, "create"]);
+Route::post("/inscribir/maestro", [ClaseController::class, "create"]);
+//
+Route::get("/asistencias", [AsistenciaController::class, "index"]);
+Route::post("/asistencias/registro/alumno", [AsistenciaController::class, "asistenciaAlumno"]);
